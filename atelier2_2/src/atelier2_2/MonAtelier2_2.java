25f2e14a9f79d19a51c3f0a726642455aff8c095
Mahdi Aggoun <mahdiaggoun18>
2021-09-08 17:37:59 -0400

    1 commit

---
+++ atelier2_2/src/atelier2_2/MonAtelier2_2.java
@@ -0,0 +1,45 @@
+package atelier2_2;
+
+import tutoriels.atelier2_2.Atelier2_2;
+
+public class MonAtelier2_2 extends Atelier2_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String x = null;
+		x.equals(null);
+		
+
+	}
+
+	@Override
+	public Object fournirBouledogue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object fournirChatGoutiere() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object fournirChatPersan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object fournirDalamtien() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object fournirShihtzu() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
