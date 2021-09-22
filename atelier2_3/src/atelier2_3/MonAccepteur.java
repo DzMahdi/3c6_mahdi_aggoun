809d5b9d7480b17c4d514842be2c50ca9664dac9
Mahdi Aggoun <mahdiaggoun18>
2021-09-22 18:00:37 -0400

    Mon Accepteur complet

--- atelier2_3/src/atelier2_3/MonAccepteur.java
+++ atelier2_3/src/atelier2_3/MonAccepteur.java
@@ -1,5 +1,34 @@
 package atelier2_3;
 
-public class MonAccepteur {
+import tutoriels.atelier2_3.Accepteur;
+import tutoriels.atelier2_3.animaux.Animal;
+import tutoriels.atelier2_3.animaux.Chat;
+
+public class MonAccepteur implements Accepteur {
+
+	@Override
+	public boolean accepterLesBonsChasseurs(Animal animal) {
+	boolean accepter = true;
+		if (animal instanceof Chat) {
+		
+			Chat chat =(Chat) animal;
+			accepter = chat.getNombreOiseauxAttrapes() > 6;
+			
+		}
+		
+		return accepter;
+	}
+
+	@Override
+	public boolean accepterLesChats(Animal animal) {
+		
+		return animal instanceof Chat;
+	}
+
+	@Override
+	public boolean accepterLesJappeurs(Animal animal) {
+	
+		return animal.typeDeCri() == "jappement";
+	}
 
 }
--- atelier2_3/src/atelier2_3/MonAtelier2_3.java
+++ atelier2_3/src/atelier2_3/MonAtelier2_3.java
@@ -1,5 +1,21 @@
 package atelier2_3;
 
-public class MonAtelier2_3 {
+import tutoriels.atelier2_3.Accepteur;
+import tutoriels.atelier2_3.Atelier2_3;
+import tutoriels.atelier2_3.Formateur;
+
+public class MonAtelier2_3 extends Atelier2_3 {
+
+	@Override
+	public Accepteur fournirAccepteur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Formateur fournirFormateur() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
